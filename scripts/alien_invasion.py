deb8940139178a5eceee8ce863b6434d61194e6a
Junior Quintana <jquintana@farelogix.com>
2019-09-06 18:06:45 -0400

    Drawing the Ship in the screen

--- scripts/alien_invasion.py
+++ scripts/alien_invasion.py
@@ -1,13 +1,20 @@
+#packages to import 
+
 import sys
 import pygame
 
+
+#scripts and classes to import
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
 
     while True:
         # Watch for keyboard and mouse events.
@@ -17,6 +24,7 @@ def run_game():
             
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
