4b4bf126a7bb4077a3a0bc32746cceeb5f61b41a
Junior Quintana <jquintana@farelogix.com>
2019-09-16 10:57:53 -0400

    Making the ship move

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,7 @@ def run_game():
 
     while True:
 
-        gf.check_events()
+        gf.check_events(ship)
 
         gf.update_screen(ai_settings, screen, ship)
         
--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,15 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                #Move the ship to the right
+                ship.rect.centerx += 1
 
 def update_screen(ai_settings, screen, ship):
      # Redraw the screen during each pass through the loop.
