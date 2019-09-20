f2bd877bd4482bfa736713de3079f60a99a6f574
Junior Quintana <jquintana@farelogix.com>
2019-09-20 17:37:27 -0400

    Controlling the ship, not going outside the screen

--- game_functions.py
+++ game_functions.py
@@ -2,15 +2,34 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        #Move the ship to the right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        #Move the ship to the left
+        ship.moving_left = True
+
+def check_keyup_events(event,ship):
+    #Respond to key releases
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                #Move the ship to the right
-                ship.rect.centerx += 1
+            check_keydown_events(event, ship)
+                
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+            
 
 def update_screen(ai_settings, screen, ship):
      # Redraw the screen during each pass through the loop.
